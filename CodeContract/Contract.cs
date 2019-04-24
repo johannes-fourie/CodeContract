c1b39a1ad7eac1a8c74668d736d84a48604e7cc5
Maree Fourie <MareeF@k2.com>
2019-04-24 21:41:54 +0200

    updated

--- CodeContract/Contract.cs
+++ CodeContract/Contract.cs
@@ -1,23 +1,37 @@
 ﻿using System;
-using System.Collections;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Runtime.CompilerServices;
+using System.Security;
 
 namespace CodeContract
 {
     public class Contract
     {
-        public Dictionary<string, object> OldValue { get; } = new Dictionary<string, object>();
+        private string _eventSource;
+
+        public Contract()
+        {
+            CreateEventSource(System.AppDomain.CurrentDomain.FriendlyName);
+        }
+
+        private enum check
+        {
+            pre,
+            post,
+            invariante
+        }
 
         public List<(string name, object preValue, Func<object> getPostValue)> Invariantes { get; } = new List<(string, object, Func<object>)>();
+
+        public Dictionary<string, object> OldValue { get; } = new Dictionary<string, object>();
 
         public void CheckInvarianets(
             [CallerFilePath] string callerFilePath = "",
             [CallerLineNumber] int callerLineNumber = -1,
             [CallerMemberName] string callerMemberName = "")
         {
-            foreach(var invariante in Invariantes)
+            foreach (var invariante in Invariantes)
             {
                 var postValue = invariante.getPostValue();
                 if (invariante.preValue != postValue)
@@ -32,36 +46,6 @@ namespace CodeContract
             }
         }
 
-        private enum check
-        {
-            pre,
-            post,
-            invariante
-        }
-
-        private void LogContractViolationEvent(
-            string message,
-            string callerFilePath,
-            int callerLineNumber,
-            string callerMemberName,
-            check check)
-        {
-            Debug.WriteLine($"[{check}], [{callerFilePath}], [{callerLineNumber}], [{callerMemberName}], [{message}]");
-        }
-
-        public void PreCondition(
-            bool check, 
-            string message = null, 
-            [CallerFilePath] string callerFilePath = "", 
-            [CallerLineNumber] int callerLineNumber = -1,
-            [CallerMemberName] string callerMemberName = "")
-        {
-            if(!check)
-            {
-                LogContractViolationEvent(message, callerFilePath, callerLineNumber, callerMemberName, Contract.check.pre);                
-            }
-        }
-
         public void PostCondition(
             bool check,
             string message = null,
@@ -73,6 +57,50 @@ namespace CodeContract
             {
                 LogContractViolationEvent(message, callerFilePath, callerLineNumber, callerMemberName, Contract.check.post);
             }
+        }
+
+        public void PreCondition(
+            bool check,
+            string message = null,
+            [CallerFilePath] string callerFilePath = "",
+            [CallerLineNumber] int callerLineNumber = -1,
+            [CallerMemberName] string callerMemberName = "")
+        {
+            if (!check)
+            {
+                LogContractViolationEvent(message, callerFilePath, callerLineNumber, callerMemberName, Contract.check.pre);
+            }
+        }
+
+        private void CreateEventSource(string currentAppName)
+        {
+            _eventSource = currentAppName;
+            try
+            {
+                if (!EventLog.SourceExists(_eventSource))
+                {
+                    EventLog.CreateEventSource(_eventSource, "Application");
+                }
+            }
+            catch (SecurityException)
+            {
+                _eventSource = "Application";
+            }
+        }
+
+        private void LogContractViolationEvent(
+            string message,
+            string callerFilePath,
+            int callerLineNumber,
+            string callerMemberName,
+            check check)
+        {
+            var logMessage = $"Contract violation: [{check}], [{callerFilePath}], [{callerLineNumber}], [{callerMemberName}], [{message}]";
+
+            new EventLog("Application") { Source = _eventSource }
+            .WriteEntry(message: logMessage, type: EventLogEntryType.Warning);
+
+            Debug.WriteLine(logMessage);
         }
     }
 }
--- Test/Program.cs
+++ Test/Program.cs
@@ -1,5 +1,10 @@
 ﻿using System;
+using System.Collections.Generic;
 using CodeContract;
+using System.Linq;
+using System.Reflection;
+using FsCheck;
+using System.Diagnostics;
 
 namespace Test
 {
@@ -7,32 +12,165 @@ namespace Test
     {
         static void Main(string[] args)
         {
-            new testing().SomeMethod(10);
+            new AClass().SomeMethod(-70);
 
+            Console.WriteLine("---------------------------");
             Console.ReadKey();
         }
 
-        public class testing
+
+        public static class Test
         {
+            public static void Check(
+                Func<bool> when, 
+                Func<bool> then, 
+                string message)
+            {
+                if (when() && !then())
+                    Debug.WriteLine(message);
+            }
+        }
+
+        public class AClass
+        {
+            public enum Time
+            {
+                Morning,
+                Afternoon,
+                Evening
+            }
+
+            public static class University
+            {
+                public static readonly string Harvard = "Harvard University";
+                public static readonly string CaliforniaBerkeley = "University of California–Berkeley";
+                public static readonly string Cambridge = "University of Cambridge";
+            }
+
+
             public string Name { get; set; }
 
-            public int SomeMethod(int input)
+            public (string m, double d) SomeCode(bool b, Time t, string u, int l, int i, int j)
+            {
+                string message;
+                double tax;
+
+                // code
+
+                return (m: message, d: tax);
+            }
+
+            public int SomeMethod(int aNumber)
+            {
+
+                var levels = new List<int>() { -10, -5, 0, 1, 30 };
+
+                var b =
+                    from boolean in new bool[] { true, false }
+                    select boolean;
+
+                var t =
+                    from time in
+                        Enum
+                        .GetValues(typeof(Time))
+                        .Cast<Time>()
+                    select time;
+
+                var u =
+                    from university in
+                        typeof(University)
+                        .GetFields(BindingFlags.Static | BindingFlags.Public)
+                        .Select(fi => (string)fi.GetValue(null))
+                    select university;
+
+                var discreateTestPoints =
+                    from boolean in new bool[] { true, false }
+                    from time in
+                        Enum
+                        .GetValues(typeof(Time))
+                        .Cast<Time>()
+                    from university in
+                        typeof(University)
+                        .GetFields(BindingFlags.Static | BindingFlags.Public)
+                        .Select(fi => (string)fi.GetValue(null))
+                    from level in levels
+                    select (b: boolean, t: time, u: university, l: level);
+
+                var i =
+                    Arb
+                    .From<int>()
+                    .Filter(integer => integer >= 1 && integer < 100)
+                    .Generator;
+
+                var j =
+                    Arb
+                    .From<int>()
+                    .Filter(integer => (integer >= 1 && integer <= 50) || (integer >= 60 && integer <= 100))
+                    .Generator;
+
+                var config = FsCheck.Configuration.Quick;
+                config.MaxNbOfTest = 5;
+
+                foreach (var discreateTestPoint in discreateTestPoints)
+                {
+                    Prop
+                        .ForAll(
+                            Arb.From(i),
+                            Arb.From(j),
+                            (ii, jj) =>
+                            {
+                                var result = SomeCode(
+                                    discreateTestPoint.b,
+                                    discreateTestPoint.t,
+                                    discreateTestPoint.u,
+                                    discreateTestPoint.l,
+                                    ii,
+                                    jj);
+
+                                Test.Check(
+                                    when: () => discreateTestPoint.b,
+                                    then: () => result.d > 10,
+                                    message: "if b then d > 10");
+
+                                Test.Check(
+                                    when: () => ii > 50,
+                                    then: () => result.m == "Tax due",
+                                    message: "if b then d > 10");
+                            })
+                        .Check(config);
+                }
+
+
+                var contract = new Contract();
+                contract.Invariantes.Add((
+                    name: nameof(Name), 
+                    preValue: Name, 
+                    getPostValue: () => Name));
+
+                // code that changes aNumber
+
+                contract.CheckInvarianets();
+                return aNumber;
+            }
+            /*
+            public int SomeMethod(int aNumber)
             {
 #if DEBUG
                 var contract = new Contract();
                 contract.Invariantes.Add((nameof(Name), Name, () => Name));
-                contract.OldValue.Add(nameof(input), input);
-                contract.PreCondition(input > 0 && input < 100, $"input: 0..100: {input}");
+                contract.OldValue.Add(nameof(aNumber), aNumber);
+                contract.PreCondition(aNumber > 0 && aNumber < 100, $"input: 0..100: {aNumber}");
 #endif
-                input = 55;
+                //input = 55;
                 Name = "ffff";
 #if DEBUG
-                contract.PostCondition((int)contract.OldValue[nameof(input)] != input, "The value of input should change");
-                contract.PostCondition(input > 50 && input < 60, $"output: 50..60: {input}");
+                contract.PostCondition((int)contract.OldValue[nameof(aNumber)] != aNumber, "The value of input should change");
+                contract.PostCondition(aNumber > 50 && aNumber < 60, $"output: 50..60: {aNumber}");
                 contract.CheckInvarianets();
 #endif
-                return input;
+                return aNumber;
             }
+            */
         }
     }
 }
